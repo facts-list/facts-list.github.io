36c7d6efe1536a96b3aad08158a54e62c64789de
Hal Finkel <hal.finkel.oss@gmail.com>
2024-01-04 02:58:49 -0500

    Adding vuestic

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ["@vuestic/nuxt"],
+
+  vuestic: {
+    config: {
+      // Config here
+    },
+  },
+
   devtools: { enabled: true }
 })
